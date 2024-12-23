30999e6f5c9c885dd416e01f767b93bf97213900
Xuanhao <shirou-emiya2002@outlook.com>
2024-12-23 20:07:06 +0100

    Add: New Node

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,70 @@
 
     <div>
       <h2>New Node</h2>
-      <textarea v-model="config" placeholder="Enter config JSON"></textarea>
+      <div>
+        <label>Identifier Length:</label>
+        <input v-model="config.IdentifierLength" type="number" placeholder="Enter Identifier Length">
+      </div>
+      <div>
+        <label>Successors Length:</label>
+        <input v-model="config.SuccessorsLength" type="number" placeholder="Enter Successors Length">
+      </div>
+      <div>
+        <label>IP Address:</label>
+        <input v-model="config.IpAddress" type="text" placeholder="Enter IP Address">
+      </div>
+      <div>
+        <label>Port:</label>
+        <input v-model="config.Port" type="number" placeholder="Enter Port">
+      </div>
+      <div>
+        <label>Mode:</label>
+        <input v-model="config.Mode" type="text" placeholder="Enter Mode">
+      </div>
+      <div>
+        <label>Join Address:</label>
+        <input v-model="config.JoinAddress" type="text" placeholder="Enter Join Address">
+      </div>
+      <div>
+        <label>Join Port:</label>
+        <input v-model="config.JoinPort" type="number" placeholder="Enter Join Port">
+      </div>
+      <div>
+        <label>Stabilize Time:</label>
+        <input v-model="config.StabilizeTime" type="number" placeholder="Enter Stabilize Time">
+      </div>
+      <div>
+        <label>Fix Fingers Time:</label>
+        <input v-model="config.FixFingersTime" type="number" placeholder="Enter Fix Fingers Time">
+      </div>
+      <div>
+        <label>Check Predecessor Time:</label>
+        <input v-model="config.CheckPredecessorTime" type="number" placeholder="Enter Check Predecessor Time">
+      </div>
+      <div>
+        <label>AES Enabled:</label>
+        <input v-model="config.AESBool" type="checkbox">
+      </div>
+      <div>
+        <label>AES Key Path:</label>
+        <input v-model="config.AESKeyPath" type="text" placeholder="Enter AES Key Path">
+      </div>
+      <div>
+        <label>TLS Enabled:</label>
+        <input v-model="config.TLSBool" type="checkbox">
+      </div>
+      <div>
+        <label>CA Certificate Path:</label>
+        <input v-model="config.CaCert" type="text" placeholder="Enter CA Certificate Path">
+      </div>
+      <div>
+        <label>Server Certificate Path:</label>
+        <input v-model="config.ServerCert" type="text" placeholder="Enter Server Certificate Path">
+      </div>
+      <div>
+        <label>Server Key Path:</label>
+        <input v-model="config.ServerKey" type="text" placeholder="Enter Server Key Path">
+      </div>
       <button @click="newNode">New Node</button>
     </div>
 
@@ -47,10 +110,29 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      config: '',
+      config: {
+        IdentifierLength: 10,
+        SuccessorsLength: 4,
+        IpAddress: '127.0.0.1',
+        Port: 4170,
+        Mode: 'create',
+        JoinAddress: '',
+        JoinPort: '',
+        StabilizeTime: 3000,
+        FixFingersTime: 1000,
+        CheckPredecessorTime: 1000,
+        AESBool: false,
+        AESKeyPath: '',
+        TLSBool: false,
+        CaCert: '',
+        ServerCert: '',
+        ServerKey: ''
+      },
       nodeState: '',
       lookupFilename: '',
       lookupResult: '',
@@ -64,7 +146,8 @@ export default {
     async newNode() {
       try {
         this.error = null;
-        await axios.post('/new', JSON.parse(this.config));
+        console.log('Sending request to create new node with config:', this.config);
+        await axios.post('/new', this.config);
         await axios.get('/initialize');
       } catch (err) {
         this.error = err.response ? err.response.data : err.message;
