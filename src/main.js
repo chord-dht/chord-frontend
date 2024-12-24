6cf3bdbdd49e85af2da8293fdb8c776f3e2e93d9
Xuanhao <shirou-emiya2002@outlook.com>
2024-12-24 18:37:28 +0100

    localhost

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 import { createApp } from 'vue';
 import App from './App.vue';
 
-axios.defaults.baseURL = 'http://172.21.255.242:8080'
+axios.defaults.baseURL = 'http://localhost:8080'
 
 createApp(App).mount('#app')
